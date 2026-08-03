27dcc7bbd018a793057bc4ea2412169095b62cd8
Renata-Ren78 <renatecertoka@yahoo.com>
2026-08-03 23:34:46 +0300

    SelectorsXpath

--- src/main/java/start/SelectorsXpath.java
+++ src/main/java/start/SelectorsXpath.java
@@ -1,8 +1,12 @@
 package start;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class SelectorsXpath {
     WebDriver driver = new ChromeDriver();
@@ -11,10 +15,75 @@ public class SelectorsXpath {
     public void phoneBookTests(){
         driver.get("https://telranedu.web.app/home");
         driver.manage().window().maximize();
-        pause(2000);
+        pause(3000);
+
+        WebElement btnAbout = driver.findElement
+                (By.xpath("//a[@href='/about\']")); // for atribut @
+        btnAbout.click();
+        pause(3000);
+        System.out.println(btnAbout.isEnabled()); // isEnabled -prisutstvuesh
+        System.out.println(btnAbout.isDisplayed());
+
+        WebElement btnLogin = driver.findElement
+                (By.xpath("//*[text() ='LOGIN']")); // * -> ljuboj element
+        btnLogin.click();
+        pause(3000);
+
+        WebElement inputEmail = driver.findElement
+                (By.xpath("//div/form/input[contains(@name, 'em')]"));
+        inputEmail.sendKeys("sdregdfshwe@gdjf.bnj");
+        pause(3000);
+
+        WebElement inputPassword = driver.findElement
+                (By.xpath("//div/form/input[last()]"));
+        inputPassword.sendKeys("Qwetry1234!!");
+        pause(3000);
+
+        List<WebElement> buttons = driver.findElements
+                (By.xpath("//button"));
+        System.out.println(buttons);
+        System.out.println(buttons.get(0).getText());
+        pause(3000);
+        buttons.get(1).click();
+        pause(3000);
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void ilCarroXPathTest(){
+        //driver.get("https://ilcarro.web.app/search");
+        driver.get("https://ilcarro.web.app/registration?url=%2Fsearch");
+        driver.manage().window().maximize();
+
+//        WebElement inputFirstName = driver.findElement
+//                (By.className("ng-pristine"));
+//        inputFirstName.sendKeys("Renata");
+//
+//        WebElement el1 = driver.findElement
+//                (By.cssSelector("[formcontrolname='firstName']"));
+
+//        WebElement el2 = driver.findElement
+//                (By.xpath("//*[@formcontrolname='firstName']"));
+//
+//        WebElement el3 = driver.findElement
+//                (By.cssSelector("[formcontrolname^='first']"));
+//
+//        WebElement el4 = driver.findElement
+//                (By.xpath("//*[starts-with(@formcontrolname,'first')]"));
+//
+//        WebElement el5 = driver.findElement
+//                (By.xpath("//input[@formcontrolname='firstName']"));
+
+        WebElement el6 = driver.findElement
+                (By.xpath("//input[@id='name']/../../../../h2"));
+        System.out.println(el6.getText());
 
 
 
+
+        pause(3000);
         driver.quit();
 
     }
